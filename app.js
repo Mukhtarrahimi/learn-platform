7c40e4217d7ef2626860586ac941e08729f9dd54
= <mukhtarrahimi.dev@gmail.com>
2025-11-27 11:15:36 +0430

    app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+
+// Route 404
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: 'Route Not Found',
+  });
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || 'Server Error',
+  });
+});
+
+module.exports = app;

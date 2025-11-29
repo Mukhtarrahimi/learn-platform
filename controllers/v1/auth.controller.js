378630247c0224282c3d75149255552ae481d851
= <mukhtarrahimi.dev@gmail.com>
2025-11-29 09:16:40 +0430

    getMe controller

--- controllers/v1/auth.controller.js
+++ controllers/v1/auth.controller.js
@@ -94,4 +94,29 @@ const login = async (req, res) => {
   }
 };
 
-module.exports = { register, login };
+// GET ME
+const getMe = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId).select(
+      '-hash_password -refresh_Token'
+    );
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: 'user not found',
+      });
+    }
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      message: 'Error in login API',
+    });
+  }
+};
+module.exports = { register, login, getMe };
--- routes/v1/auth.route.js
+++ routes/v1/auth.route.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { register } = require('../../controllers/v1/auth.controller');
+const { register, login } = require('../../controllers/v1/auth.controller');
 const validate = require('../../middlewares/validate');
-const { registerSchema } = require('../../validators/user.validator');
+const {
+  registerSchema,
+  loginSchema,
+} = require('../../validators/user.validator');
 
 // REGISTER
 router.post('/register', validate(registerSchema), register);
+// LOGIN
+router.post('/login', validate(loginSchema), login);
 
 module.exports = router;

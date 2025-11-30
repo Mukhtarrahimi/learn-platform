b1d9e089b142eb6386a598973de4297e330235aa
= <mukhtarrahimi.dev@gmail.com>
2025-11-30 09:51:41 +0430

    getUser byId route

--- routes/v1/admin.route.js
+++ routes/v1/admin.route.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const verifyToken = require('../../middlewares/verifyToken');
 const checkUserStatus = require('../../middlewares/checkUserStatus');
 const checkAdmin = require('../../middlewares/checkAdmin');
-const { getAllUser } = require('../../controllers/v1/admin.controller');
+const {
+  getAllUser,
+  getUserById,
+} = require('../../controllers/v1/admin.controller');
 
 // IS ADMIN ----------------------
 router.get('/users', verifyToken, checkAdmin, getAllUser);
+router.get('/users/:id', verifyToken, checkAdmin, getUserById);
 
 module.exports = router;

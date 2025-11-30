8f7e0adbacf227c2817983f537ef366d882e7754
= <mukhtarrahimi.dev@gmail.com>
2025-11-30 10:01:01 +0430

    admin route update and getUserByQuery added

--- routes/v1/admin.route.js
+++ routes/v1/admin.route.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const verifyToken = require('../../middlewares/verifyToken');
-const checkUserStatus = require('../../middlewares/checkUserStatus');
 const checkAdmin = require('../../middlewares/checkAdmin');
 const {
   getAllUser,
   getUserById,
+  getUserByQuery,
 } = require('../../controllers/v1/admin.controller');
 
-// IS ADMIN ----------------------
-router.get('/users', verifyToken, checkAdmin, getAllUser);
+// Admin routes
+router.get('/users', verifyToken, checkAdmin, getUserByQuery); // query param
+router.get('/users/all', verifyToken, checkAdmin, getAllUser);
 router.get('/users/:id', verifyToken, checkAdmin, getUserById);
 
 module.exports = router;

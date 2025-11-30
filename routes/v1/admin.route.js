c65a34cd6c16156d322d3bbd94a8732fbc6287e6
= <mukhtarrahimi.dev@gmail.com>
2025-11-30 10:18:54 +0430

    delete user add to admin route

--- routes/v1/admin.route.js
+++ routes/v1/admin.route.js
@@ -6,11 +6,19 @@ const {
   getAllUser,
   getUserById,
   getUserByQuery,
+  deleteUser,
 } = require('../../controllers/v1/admin.controller');
 
-// Admin routes
-router.get('/users', verifyToken, checkAdmin, getUserByQuery); // query param
-router.get('/users/all', verifyToken, checkAdmin, getAllUser);
+// GET all users
+router.get('/users', verifyToken, checkAdmin, getAllUser);
+
+// GET user by ID
 router.get('/users/:id', verifyToken, checkAdmin, getUserById);
+
+// GET user by query (username, email, status)
+router.get('/users/search', verifyToken, checkAdmin, getUserByQuery);
+
+// DELETE user by id or email
+router.delete('/users/delete', verifyToken, checkAdmin, deleteUser);
 
 module.exports = router;

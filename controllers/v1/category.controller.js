58d6a19cc546995bb7da21a46a978f0a06d06f15
= <mukhtarrahimi.dev@gmail.com>
2025-12-04 09:38:46 +0430

    category.controller.js createnew category

---
+++ controllers/v1/category.controller.js
@@ -0,0 +1,32 @@
+const Category = require('../../models/category.model');
+
+// Create a new category
+const createCategory = async (req, res) => {
+  try {
+    const { title, description, slug, status } = req.body;
+    const category = await Category.create({
+      title,
+      description,
+      slug,
+      status,
+    });
+    if (!category) {
+      return res.status(400).json({
+        success: false,
+        message: 'category creation failed',
+      });
+    }
+    res.status(201).json({
+      success: true,
+      message: 'category created successfully',
+      category,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: 'error in creating category api',
+      error: err.message,
+    });
+  }
+};

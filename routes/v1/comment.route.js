7e0537b6828ca7d4cd874ed187936bc8dca46cbc
= <mukhtarrahimi.dev@gmail.com>
2025-12-05 17:53:23 +0430

    change comment status route

--- routes/v1/comment.route.js
+++ routes/v1/comment.route.js
@@ -4,13 +4,17 @@ const router = express.Router();
 const { createCommentSchema } = require('../../validators/comment.validator');
 const validate = require('../../middlewares/validate');
 const verifyToken = require('../../middlewares/verifyToken');
+const checkAdmin = require('../../middlewares/checkAdmin');
 const {
   createComment,
   getCommentsByCourse,
+  changeCommentStatus,
 } = require('../../controllers/v1/comment.controller');
 
 // POST -> Create New Comment
 router.post('/', verifyToken, validate(createCommentSchema), createComment);
 // GET -> Get Comments By Course ID
 router.get('/course/:courseId', verifyToken, getCommentsByCourse);
+// PUT -> Change Comment Status
+router.put('/:commentId/status', verifyToken, checkAdmin, changeCommentStatus);
 module.exports = router;

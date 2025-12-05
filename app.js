743b5acbcb16a7765a1da9ce86e0a99623ef1ab8
= <mukhtarrahimi.dev@gmail.com>
2025-12-05 11:27:32 +0430

    comment route add to app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const adminRoute = require('./routes/v1/admin.route');
 const courseRoute = require('./routes/v1/course.route');
 const lessonRoute = require('./routes/v1/lesson.route');
 const categoryRoute = require('./routes/v1/category.route');
+const commentRoute = require('./routes/v1/comment.route');
 
 // Middleware
 app.use(express.json());
@@ -24,6 +25,7 @@ app.use('/api/v1/admin', adminRoute);
 app.use('/api/v1/courses', courseRoute);
 app.use('/api/v1/lessons', lessonRoute);
 app.use('/api/v1/categories', categoryRoute);
+app.use('/api/v1/comments', commentRoute);
 
 // Route 404
 app.use((req, res, next) => {

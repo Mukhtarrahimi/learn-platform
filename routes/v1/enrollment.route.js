ea1547ac7bd3b06d3af1af496eb5351f7410f878
= <mukhtarrahimi.dev@gmail.com>
2025-12-06 08:18:05 +0430

    getAll enrollment course

--- routes/v1/enrollment.route.js
+++ routes/v1/enrollment.route.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { enrollCourse } = require('../../controllers/v1/enrollment.controller');
+
+// Middleware
 const validate = require('../../middlewares/validate');
 const verifyToken = require('../../middlewares/verifyToken');
+// Validator
 const { enrollCourseSchema } = require('../../validators/enrollment.validator');
+// Controller
+const {
+  enrollCourse,
+  getAllEnrollCourse,
+} = require('../../controllers/v1/enrollment.controller');
 // POST -> Enroll in a Course
 router.post('/enroll', verifyToken, validate(enrollCourseSchema), enrollCourse);
+// GET -> Get All Enrollments
+router.get('/', verifyToken, getAllEnrollCourse);
 module.exports = router;

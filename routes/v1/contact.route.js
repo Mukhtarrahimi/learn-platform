2cf4c2087f7c0c58080c9a015972f3999e27e483
= <mukhtarrahimi.dev@gmail.com>
2025-12-07 14:58:47 +0430

    remove contact route

--- routes/v1/contact.route.js
+++ routes/v1/contact.route.js
@@ -2,15 +2,19 @@ const express = require('express');
 const router = express.Router();
 // middleware
 const validate = require('../../middlewares/validate');
+const checkAdmin = require('../../middlewares/checkAdmin');
 // Validator
 const { createContactSchema } = require('../../validators/contact.validator');
 // Controller
 const {
   createContact,
   getAllContacts,
+  removeContact,
 } = require('../../controllers/v1/contact.controller');
 // Create Contact
 router.post('/', validate(createContactSchema), createContact);
 // Get Contacts
 router.get('/', getAllContacts);
+// Remove Contact - Admin Only
+router.delete('/:contactId', checkAdmin, removeContact);
 module.exports = router;

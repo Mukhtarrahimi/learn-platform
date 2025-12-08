9e802c7946e68e9e497b854d68f8f69778778ba9
= <mukhtarrahimi.dev@gmail.com>
2025-12-08 09:14:00 +0430

    getFavorite courses route

--- routes/v1/favoriteCourse.route.js
+++ routes/v1/favoriteCourse.route.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const {
   addFavoriteCourse,
   deleteFavoriteCourse,
+  getFavoriteCourses,
 } = require('../../controllers/v1/favoriteCourse.controller');
 const verifyToken = require('../../middlewares/verifyToken');
 
 // Add a course to favorites
 router.post('/', verifyToken, addFavoriteCourse);
 module.exports = router;
+// Get all favorite courses
+router.get('/', verifyToken, getFavoriteCourses);
 // Delete a course from favorites
 router.delete('/:courseId', verifyToken, deleteFavoriteCourse);
 module.exports = router;

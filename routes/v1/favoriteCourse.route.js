526d74537e624ff4202c9f6e8c4affd9bc5fa3aa
= <mukhtarrahimi.dev@gmail.com>
2025-12-08 08:59:45 +0430

    deleteFavorite course route

--- routes/v1/favoriteCourse.route.js
+++ routes/v1/favoriteCourse.route.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const {
   addFavoriteCourse,
+  deleteFavoriteCourse,
 } = require('../../controllers/v1/favoriteCourse.controller');
 const verifyToken = require('../../middlewares/verifyToken');
 
 // Add a course to favorites
 router.post('/', verifyToken, addFavoriteCourse);
+module.exports = router;
+// Delete a course from favorites
+router.delete('/:courseId', verifyToken, deleteFavoriteCourse);
 module.exports = router;

d4370bd93b266aab9229600b4b1a81cd10baa9d9
= <mukhtarrahimi.dev@gmail.com>
2025-12-09 11:27:35 +0430

    menu controller

---
+++ controllers/v1/menu.controller.js
@@ -0,0 +1,33 @@
+const Menu = require('../../models/menu.model');
+
+const createMenu = async (req, res) => {
+  try {
+    const { title, link, order, parentId, isActive } = req.body;
+    const menuExist = await Menu.findOne({ title, parentId: parentId || null });
+    if (menuExist) {
+      return res.status(400).json({
+        success: false,
+        message: 'menu already exists',
+      });
+    }
+    const menu = await Menu.create({
+      title,
+      link,
+      order,
+      parentId,
+      isActive,
+    });
+    res.status(201).json({
+      success: true,
+      message: 'menu created successfully',
+      menu,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: 'error creating menu',
+      error: err.message,
+    });
+  }
+};

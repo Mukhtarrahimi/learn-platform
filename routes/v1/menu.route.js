ad6d2904c870360cadcd19ad7cb386e8a00d50c2
= <mukhtarrahimi.dev@gmail.com>
2025-12-10 13:11:28 +0430

    delete menu route

--- routes/v1/menu.route.js
+++ routes/v1/menu.route.js
@@ -10,11 +10,13 @@ const { createMenuSchema } = require('../../validators/menu.validator');
 const {
   createMenu,
   updateMenu,
+  deleteMenu,
 } = require('../../controllers/v1/menu.controller');
 
 // Create Menu
 router.post('/', verify, checkAdmin, validate(createMenuSchema), createMenu);
 // Update Menu
 router.put('/:id', verify, checkAdmin, validate(createMenuSchema), updateMenu);
-
+// Delete Menu
+router.delete('/:id', verify, checkAdmin, deleteMenu);
 module.exports = router;

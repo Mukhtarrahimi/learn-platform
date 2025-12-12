b869289e19a30463e0205adf7c776b187d04e65d
= <mukhtarrahimi.dev@gmail.com>
2025-12-12 09:49:42 +0430

    category.validator update

--- validators/category.validator.js
+++ validators/category.validator.js
@@ -2,7 +2,16 @@ const Joi = require('joi');
 const createCategorySchema = Joi.object({
   title: Joi.string().min(3).max(50).required(),
   description: Joi.string().max(500).allow('', null),
-  slug: Joi.string().alphanum().min(3).max(30).required(),
+  slug: Joi.string()
+    .pattern(/^[a-z0-9]+(?:-[a-z0-9]+)*$/)
+    .required()
+    .messages({
+      'string.pattern.base':
+        'Slug must contain lowercase letters, numbers, and hyphens only.',
+    })
+    .min(3)
+    .max(30)
+    .required(),
   status: Joi.string().valid('active', 'inactive').default('active'),
 });
 

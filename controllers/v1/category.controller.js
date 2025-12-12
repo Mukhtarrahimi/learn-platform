d9c542a5d73258c3bc96229733d9de8bea2dfe47
= <mukhtarrahimi.dev@gmail.com>
2025-12-12 09:52:44 +0430

    update category controller

--- controllers/v1/category.controller.js
+++ controllers/v1/category.controller.js
@@ -31,6 +31,42 @@ const createCategory = async (req, res) => {
   }
 };
 
+const updateCategory = async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        message: 'category id is required',
+      });
+    }
+    const { title, description, slug, status } = req.body;
+    const category = await Category.findByIdAndUpdate(
+      id,
+      { title, description, slug, status },
+      { new: true }
+    );
+    if (!category) {
+      return res.status(400).json({
+        success: false,
+        message: 'category update failed',
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: 'category updated successfully',
+      category,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: 'error in creating category api',
+      error: err.message,
+    });
+  }
+};
+
 module.exports = {
   createCategory,
 };

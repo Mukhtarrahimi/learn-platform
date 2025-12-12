e64e98610997a1e4bfa626d5a7cb99180a39f113
= <mukhtarrahimi.dev@gmail.com>
2025-12-12 09:59:17 +0430

    getAll category controller

--- controllers/v1/category.controller.js
+++ controllers/v1/category.controller.js
@@ -68,7 +68,33 @@ const updateCategory = async (req, res) => {
   }
 };
 
+// Get all categories
+const getAllCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+    if (!categories) {
+      return res.status(404).json({
+        sucess: false,
+        message: 'no categories found',
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: 'categories fetched successfully',
+      categories,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: 'error in creating category api',
+      error: err.message,
+    });
+  }
+};
+
 module.exports = {
   createCategory,
   updateCategory,
+  getAllCategories,
 };
